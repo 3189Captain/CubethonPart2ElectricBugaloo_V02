a424250a58883278571764777e051fc1491d898a
3189Captain <oogabooga300@gmail.com>
2020-01-15 09:34:40 -0700

    Update Wander.cs

--- Assets/Scripts/Wander.cs
+++ Assets/Scripts/Wander.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Wander : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float speed = 25f;
-    Vector3 wayPoint;
+    public float speed = 25f; //base speed of the wander object
+    Vector3 wayPoint; //a coordinate randomly chosen
 
     // Update is called once per frame
     void Start()
@@ -15,17 +15,17 @@ public class Wander : MonoBehaviour
     }
     void Update()
     {
-        transform.position += transform.TransformDirection(Vector3.forward) * speed * Time.deltaTime;
-        if((transform.position - wayPoint).magnitude < 3)
+        transform.position += transform.TransformDirection(Vector3.forward) * speed * Time.deltaTime;//move the player towards the wayPoint
+        if((transform.position - wayPoint).magnitude < 3)//if the player is within 3 units of the wayPoint choose a new one
         {
             ChooseHeading();
         }
     }
 
-    void ChooseHeading()
+    void ChooseHeading()//picks the next wayPoint
     {
-        wayPoint = Random.insideUnitSphere * 47;
-        wayPoint.y = 1;
-        transform.LookAt(wayPoint);
+        wayPoint = Random.insideUnitSphere * 47;//multiplied by a constant to make sure the wayPoint is far awy from the player
+        wayPoint.y = 1;//y component is not nescessary
+        transform.LookAt(wayPoint);//look at the wayPoint
     }
 }
