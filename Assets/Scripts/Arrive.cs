19a7fb273cebdc3bdf8ff20609fa75e72e18f06f
3189Captain <oogabooga300@gmail.com>
2020-01-15 09:17:46 -0700

    project upload

---
+++ Assets/Scripts/Arrive.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrive : MonoBehaviour
+{
+    public GameObject player; //reference to player
+    public float speed; //multiplier for AI travel speed
+    Vector3 playerVector; //vector calculated by taking the difference between this object's postion and the player
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed = 5f;
+        playerVector = player.transform.position;
+        playerVector.y = 1;
+        transform.position += transform.TransformDirection(Vector3.forward) * speed * Time.deltaTime;
+        transform.LookAt(playerVector);
+
+        if ((player.transform.position - transform.position).magnitude < 10)
+        {
+            speed = speed / 2;
+            transform.position += transform.TransformDirection(Vector3.forward) * speed * Time.deltaTime;
+            transform.LookAt(playerVector);
+        }
+        if((player.transform.position - transform.position).magnitude < 5)
+        {
+            speed = speed / 10;
+            transform.position += transform.TransformDirection(Vector3.forward) * speed * Time.deltaTime;
+            transform.LookAt(playerVector);
+            Debug.Log("made it!");
+        }
+    }
+}
---
+++ Assets/Scripts/Wander.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wander : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 25f;
+    Vector3 wayPoint;
+
+    // Update is called once per frame
+    void Start()
+    {
+        ChooseHeading();
+    }
+    void Update()
+    {
+        transform.position += transform.TransformDirection(Vector3.forward) * speed * Time.deltaTime;
+        if((transform.position - wayPoint).magnitude < 3)
+        {
+            ChooseHeading();
+        }
+    }
+
+    void ChooseHeading()
+    {
+        wayPoint = Random.insideUnitSphere * 47;
+        wayPoint.y = 1;
+        transform.LookAt(wayPoint);
+    }
+}
